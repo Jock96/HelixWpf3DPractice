f2f660a12f238db4bbd477aab8b22d6df5e116fe
Jock_96 <Peter96pvl@gmail.com>
2019-07-31 20:41:39 +0700

    # 006
    Добавлено изображение изолиний. Добавлено сглаживание. Добавлено построение моделей по изолиниям.

--- Isolines3D/Isolines3D.UI/Commands/CreateByIsolinesCommand.cs
+++ Isolines3D/Isolines3D.UI/Commands/CreateByIsolinesCommand.cs
@@ -3,9 +3,11 @@
     using Isolines3D.UI.Extensions;
     using Isolines3D.UI.Helpers;
     using Isolines3D.UI.ViewModels;
+    using Microsoft.Win32;
     using System;
     using System.Drawing;
     using System.IO;
+    using System.Windows;
 
     /// <summary>
     /// Команда формирования модели по изолиниям.
@@ -18,152 +20,32 @@
         /// <param name="mainWindowVM">Вью-модель главного окна.</param>
         protected override void Execute(MainWindowVM mainWindowVM)
         {
-            var resourcesPath = PathHelper.GetResourcesPath() + "\\Example.bmp";
-            var random = new Random();
-
-            var width = (int)mainWindowVM.BasePlane.Width;
-            var length = (int)mainWindowVM.BasePlane.Length;
-
-            //var width = 20;
-            //var length = 20;
-
-            var startPointX = random.Next(width + 1);
-            var startPointY = random.Next(length + 1);
-
-            using (var bitmap = new Bitmap(width + 1, length + 1))
+            var fileDialog = new OpenFileDialog
             {
-                var color = Color.FromArgb(byte.MaxValue, byte.MinValue, byte.MinValue);
+                Filter = @"(Isoline*.bmp)|Isoline*.bmp",
+                InitialDirectory = PathHelper.GetResourcesPath()
+            };
 
-                // Центральная точка.
-                bitmap.TrySetPixel(startPointX, startPointY, color);
+            var file = fileDialog.ShowDialog();
 
-                int maxPixelIndex = byte.MaxValue + 1;
-                var colorOffset = 5;
+            if (!file.HasValue || string.Equals(string.Empty, fileDialog.FileName))
+                return;
 
-                for (var index = 0; index < maxPixelIndex / colorOffset; ++index)
+            try
+            {
+                using (var fileStream = fileDialog.OpenFile())
+                using (var bitmap = new Bitmap(fileStream))
                 {
-                    //FillColorMap(width, length, bitmap);
-                    FillColorMap(width, length, bitmap, colorOffset);
+                    mainWindowVM.ImageSource = bitmap.ConvertToImageSource();
+                    /*mainWindowVM.Model = mainWindowVM.ModelCreatorUtil.CreateModelByImage(bitmap)*/;
+
+                    mainWindowVM.Model = mainWindowVM.ModelCreatorUtil.CreateModelByIsolines(bitmap);
                 }
-
-                // Для посмотра созданной карты виртуальных изолиний.
-                bitmap.Save(resourcesPath);
-
-                mainWindowVM.ImageSource = bitmap.ConvertToImageSource();
-                mainWindowVM.Model = mainWindowVM.ModelCreatorUtil.CreateModelByImage(bitmap);
             }
-        }
-
-        private static void FillColorMap(int width, int length, Bitmap bitmap, int colorOffset = 0)
-        {
-            var random = new Random();
-
-            for (var indexX = 0; indexX < width + 1; ++indexX)
+            catch (Exception exception)
             {
-                for (var indexY = 0; indexY < length + 1; ++indexY)
-                {
-                    var currentPixel = bitmap.GetPixel(indexX, indexY);
-
-                    if (currentPixel.R == 0)
-                        continue;
-
-                    var newColor = Color.White;
-
-                    try
-                    {
-                        newColor = Color.FromArgb(currentPixel.R - colorOffset, currentPixel.G, currentPixel.B);
-                    }
-                    catch
-                    {
-                        continue;
-                    }
-
-                    var nextPixel = Color.White;
-
-                    // Нижний пиксель.
-                    if (bitmap.IsPixelExist(indexX, indexY - 1))
-                        nextPixel = bitmap.GetPixel(indexX, indexY - 1);
-
-                    if (nextPixel.R == 0)
-                    {
-                        var gridCollapse = random.Next(colorOffset);
-                        var deltaFactor = random.Next(-1, 2);
-                        
-                        try
-                        {
-                            newColor = Color.FromArgb(currentPixel.R - colorOffset + deltaFactor * gridCollapse, currentPixel.G, currentPixel.B);
-                        }
-                        catch
-                        {
-                            // Ignore.
-                        }
-
-                        bitmap.TrySetPixel(indexX, indexY - 1, newColor);
-                    }
-
-                    // Верхний пиксель.
-                    if (bitmap.IsPixelExist(indexX, indexY + 1))
-                        nextPixel = bitmap.GetPixel(indexX, indexY + 1);
-
-                    if (nextPixel.R == 0)
-                    {
-                        var gridCollapse = random.Next(colorOffset);
-                        var deltaFactor = random.Next(-1, 2);
-
-                        try
-                        {
-                            newColor = Color.FromArgb(currentPixel.R - colorOffset + deltaFactor * gridCollapse, currentPixel.G, currentPixel.B);
-                        }
-                        catch
-                        {
-                            // Ignore.
-                        }
-
-                        bitmap.TrySetPixel(indexX, indexY + 1, newColor);
-                    }
-
-                    // Левый пиксель.
-                    if (bitmap.IsPixelExist(indexX - 1, indexY))
-                        nextPixel = bitmap.GetPixel(indexX - 1, indexY);
-
-                    if (nextPixel.R == 0)
-                    {
-                        var gridCollapse = random.Next(colorOffset);
-                        var deltaFactor = random.Next(-1, 2);
-
-                        try
-                        {
-                            newColor = Color.FromArgb(currentPixel.R - colorOffset + deltaFactor * gridCollapse, currentPixel.G, currentPixel.B);
-                        }
-                        catch
-                        {
-                            // Ignore.
-                        }
-
-                        bitmap.TrySetPixel(indexX - 1, indexY, newColor);
-                    }
-
-                    // Правый пиксель.
-                    if (bitmap.IsPixelExist(indexX + 1, indexY))
-                        nextPixel = bitmap.GetPixel(indexX + 1, indexY);
-
-                    if (nextPixel.R == 0)
-                    {
-                        var gridCollapse = random.Next(colorOffset);
-                        var deltaFactor = random.Next(-1, 2);
-
-                        try
-                        {
-                            newColor = Color.FromArgb(currentPixel.R - colorOffset + deltaFactor * gridCollapse, currentPixel.G, currentPixel.B);
-                        }
-                        catch
-                        {
-                            // Ignore.
-                        }
-
-                        bitmap.TrySetPixel(indexX + 1, indexY, newColor);
-                    }
-                }
+                MessageBox.Show($"Не удалось импортировать файл!" +
+                    $"\nОшибка: {exception.ToString()}");
             }
         }
     }
--- Isolines3D/Isolines3D.UI/Utils/ModelCreatorUtil.cs
+++ Isolines3D/Isolines3D.UI/Utils/ModelCreatorUtil.cs
@@ -2,6 +2,7 @@
 {
     using HelixToolkit.Wpf;
     using System;
+    using System.Collections.Generic;
     using System.Drawing;
     using System.Windows.Media;
     using System.Windows.Media.Media3D;
@@ -43,16 +44,24 @@
             var halfOfWidth = _modelWidth / 2;
             var halfOfLength = _modelLenght / 2;
 
+            //var pointsList = new List<List<Point3D>>();
+            var pointsArray = new List<Point3D>();
+
             for (var indexX = -halfOfWidth; indexX <= halfOfWidth; ++indexX)
             {
                 for (var indexY = -halfOfLength; indexY <= halfOfLength; ++indexY)
                 {
                     try
                     {
-                        meshBuilder.AddTriangle(
-                        new Point3D(indexX, indexY, gridMatrix[indexX + halfOfWidth, indexY + halfOfLength] * smoothingFactor),
-                        new Point3D(indexX + 1, indexY, gridMatrix[indexX + halfOfWidth + 1, indexY + halfOfLength] * smoothingFactor),
-                        new Point3D(indexX, indexY + 1, gridMatrix[indexX + halfOfWidth, indexY + halfOfLength + 1] * smoothingFactor));
+                        var pointOne = new Point3D(indexX, indexY, gridMatrix[indexX + halfOfWidth, indexY + halfOfLength] * smoothingFactor);
+                        var pointTwo = new Point3D(indexX + 1, indexY, gridMatrix[indexX + halfOfWidth + 1, indexY + halfOfLength] * smoothingFactor);
+                        var pointThree = new Point3D(indexX, indexY + 1, gridMatrix[indexX + halfOfWidth, indexY + halfOfLength + 1] * smoothingFactor);
+
+                        meshBuilder.AddTriangle(pointOne, pointTwo, pointThree);
+
+                        pointsArray.Add(pointOne);
+                        pointsArray.Add(pointTwo);
+                        pointsArray.Add(pointThree);
                     }
                     catch
                     {
@@ -61,10 +70,15 @@
 
                     try
                     {
-                        meshBuilder.AddTriangle(
-                            new Point3D(indexX + 1, indexY, gridMatrix[indexX + halfOfWidth + 1, indexY + halfOfLength] * smoothingFactor),
-                            new Point3D(indexX, indexY + 1, gridMatrix[indexX + halfOfWidth, indexY + halfOfLength + 1] * smoothingFactor),
-                            new Point3D(indexX + 1, indexY + 1, gridMatrix[indexX + halfOfWidth + 1, indexY + halfOfLength + 1] * smoothingFactor));
+                        var pointFour = new Point3D(indexX + 1, indexY, gridMatrix[indexX + halfOfWidth + 1, indexY + halfOfLength] * smoothingFactor);
+                        var pointFive = new Point3D(indexX, indexY + 1, gridMatrix[indexX + halfOfWidth, indexY + halfOfLength + 1] * smoothingFactor);
+                        var pointSix = new Point3D(indexX + 1, indexY + 1, gridMatrix[indexX + halfOfWidth + 1, indexY + halfOfLength + 1] * smoothingFactor);
+
+                        meshBuilder.AddTriangle(pointFour, pointFive, pointSix);
+
+                        pointsArray.Add(pointFour);
+                        pointsArray.Add(pointFive);
+                        pointsArray.Add(pointSix);
                     }
                     catch
                     {
@@ -72,6 +86,12 @@
                     }
                 }
             }
+
+            // Убрать, если будет виснуть.
+            meshBuilder.CreateNormals = true;
+
+            foreach (var point in pointsArray)
+                meshBuilder.AddBox(point, 1, 1, 1);
         }
 
         /// <summary>
@@ -109,6 +129,29 @@
         }
 
         /// <summary>
+        /// Создание модели по изолиниям.
+        /// </summary>
+        /// <param name="bitmap">Изображение.</param>
+        /// <param name="smoothingFactor">Сглаживание модели.</param>
+        /// <returns>Возвращает 3D модель.</returns>
+        public Model3DGroup CreateModelByIsolines(Bitmap bitmap, double smoothingFactor = 0)
+        {
+            var colorMap = ColorMapFromIsolines(bitmap);
+
+            var gridMatrix = new int[_modelWidth + 1, _modelLenght + 1];
+
+            for (var indexX = 0; indexX < _modelWidth; ++indexX)
+            {
+                for (var indexY = 0; indexY < _modelLenght; ++indexY)
+                {
+                    gridMatrix[indexX, indexY] = colorMap[indexX, indexY].R;
+                }
+            }
+
+            return BuildModel(smoothingFactor, gridMatrix);
+        }
+
+        /// <summary>
         /// Создаёт 3D модель по изображению.
         /// </summary>
         /// <param name="bitmap">Изображение.</param>
@@ -127,8 +170,21 @@
                 }
             }
 
+            return BuildModel(smoothingFactor, gridMatrix);
+        }
+
+        /// <summary>
+        /// Создать модель по сетке высот и заданым сглаживанием.
+        /// </summary>
+        /// <param name="smoothingFactor">Фактор сглаживание.</param>
+        /// <param name="gridMatrix">Сетка высот.</param>
+        /// <returns>Возвращает построенную модель.</returns>
+        private static Model3DGroup BuildModel(double smoothingFactor, int[,] gridMatrix)
+        {
             var modelGroup = new Model3DGroup();
             var meshBuilder = new MeshBuilder(false, false);
+
+            meshBuilder.CreateNormals = true;
 
             if (smoothingFactor == 0)
                 CreateTerrain(gridMatrix, meshBuilder, 0.1);
@@ -146,6 +202,79 @@
             });
 
             return modelGroup;
+        }
+
+        /// <summary>
+        /// Формирование карты по изолиниям.
+        /// </summary>
+        /// <remarks>
+        /// Работает только на тестовом варианте.
+        /// </remarks>
+        /// <param name="bitmap">Изображение.</param>
+        /// <returns>Возвращает карту цветов для преобразования в модель.</returns>
+        private System.Windows.Media.Color[,] ColorMapFromIsolines(Bitmap bitmap)
+        {
+            var colorMap = new System.Windows.Media.Color[_modelWidth + 1, _modelLenght + 1];
+
+            if (bitmap.Width != _modelWidth ||
+                bitmap.Height != _modelLenght)
+            {
+                var image = bitmap;
+                var newSize = new System.Drawing.Size(_modelWidth + 1, _modelLenght + 1);
+
+                // Поворот для корректного преобразования в битовую карту и модель.
+                image.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+                using (var resizedBitmap = new Bitmap(image, newSize))
+                {
+                    for (var indexX = 0; indexX < resizedBitmap.Width; ++indexX)
+                    {
+                        for (var indexY = 0; indexY < resizedBitmap.Height; ++indexY)
+                        {
+                            var currentPixel = resizedBitmap.GetPixel(indexX, indexY);
+
+                            var color = System.Windows.Media.Color.FromArgb(0,0,0,0);
+
+                            // Изображение залито в белом фоне цветом с содержанием R
+                            // равным: 255, 254, 253 и т.д.
+
+                            switch (currentPixel.R)
+                            {
+                                case 255:
+                                    color.R = 1;
+                                    break;
+
+                                case 254:
+                                    color.R = 45;
+                                    break;
+
+                                case 253:
+                                    color.R = 95;
+                                    break;
+
+                                case 252:
+                                    color.R = 150;
+                                    break;
+
+                                case 251:
+                                    color.R = 200;
+                                    break;
+
+                                case 250:
+                                    color.R = 250;
+                                    break;
+
+                                default:
+                                    break;
+                            }
+
+                            colorMap[indexX, indexY] = color;
+                        }
+                    }
+                }
+            }
+
+            return colorMap;
         }
 
         /// <summary>
